c6b38872d6d2150102603aa5009d753eef999094
jinwu <wu.jin@yisquare.com.cn>
2024-12-13 16:30:39 +0800

    成功上车

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="card flex justify-center gap-20">
+    <Button label="Verify" />
+    <Button label="确认"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
